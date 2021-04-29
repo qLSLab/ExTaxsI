9e74fb0aac070ed7137357c88257d69e777cf970
albertobrusati <alberto.brusati@gmail.com>
2021-04-29 13:27:18 +0200

    taxonomy files input query modified

--- ExTaxsI.py
+++ ExTaxsI.py
@@ -3,8 +3,6 @@
 #
 #  ExTaxsI.py
 #
-#  Copyright 2019 Adam <a.chahed@campus.unimib.it>
-#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
