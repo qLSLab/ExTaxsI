071cce7589ecf40e07137f37a02ec65bcaced1b1
albertobrusati <alberto.brusati@gmail.com>
2021-10-18 13:13:42 +0200

    add library folder

---
+++ library/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+setup(
+  name = 'extaxsi',
+  packages = ['extaxsi'],
+  version = '0.1',
+  license='MIT',
+  description = 'Extaxsi is a bioinformatic library aimed to elaborate and visualize molecular and taxonomic informations',
+  author = 'alberto brusati',
+  author_email = 'alberto.brusati@gmail.com',
+  url = 'https://github.com/user/extaxsi',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    
+  keywords = ['bioinformatic', 'ncbi', 'molecular data', 'visualization', 'taxonomy', 'converter'],
+  install_requires=[
+          'biopython',
+          'numpy',
+          'scipy',
+          'matplotlib',
+          'ipython',
+          'pandas',
+          'sympy',
+          'nose',
+          'plotly',
+          'ete3',
+          'kaleido'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
